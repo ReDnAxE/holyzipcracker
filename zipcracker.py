5d071240dd45de45cea2f51a970fcc1121ab0808
Alexandre GESLIN <alexandre.geslin@external.grdf.fr>
2018-08-28 11:13:55 +0200

    improve progress

--- zipcracker.py
+++ zipcracker.py
@@ -1,21 +1,21 @@
-import zipfile, itertools, sys, re
+import itertools, sys, re
+from zipfile import ZipFile
 
 passwordMangleTabRegexs=[
-    ("\n","","del new lines"),
+    ("","","no change"), #TODO: DELETE
     ("^\w{1,5} \d{1,4}:\d{1,4} ","","del prefixes"),
     ("\.","","del points"),
-    ("[^\w]+","","delete all special characters"),
+    ("[^\w]+","","delete all non alphabetic characters"),
     ("FUNCTION","TOLOWERCASE","to lowercase")
 ]
 
 
-def extract(zfile,password, description):
-    print 'test: %s (%s)' %password %description
+def extract(zfile, password, description):
+    #print (description + ' : ' + password)
     try:
-        zf = zipfile.ZipFile(zfile)
-        files = zf.infolist()
-        zf.read(files[0],pwd = password)
-        zf.close()
+        with ZipFile(zfile) as zf:
+            zf.extractall(pwd=password.encode())
+            zf.close()
         return True
     except KeyboardInterrupt:
         exit(0)
@@ -23,28 +23,27 @@ def extract(zfile,password, description):
         return False
 
 
-def manglePasswordAndExtract(zfile, password):
-    print '\nbefore mangle: %s' %password
-    global passwordMangleTabRegexs, re
+def manglePasswordAndExtract(zfile, password, passwordMangleTabRegexs):
+    global re
     for regex,replacement,description in passwordMangleTabRegexs:
         if regex == 'FUNCTION': #TODO: allow multiple functions
             password = password.lower()
         else:
             password = re.sub(regex, replacement, password)
 
-        print description
-        flag = extract(zfile, password, description)
-        if flag == True:
-            print 'mangled password found !!! %s' %password
-            return True
+        opened = extract(zfile, password, description)
+
+        if opened == True:
+            print ('mangled password found !!! ' + password)
+            return password
 
     return False
 
 
 def printRegexsUsed(passwordMangleTabRegexs):
     for x in range(len(passwordMangleTabRegexs)):
-         print ' - ' + passwordMangleTabRegexs[x][2],
-    print '' #\n
+         print (' - ' + passwordMangleTabRegexs[x][2], end='')
+    print ('\n')
 
 
 def generateRegexsPermutsPossibilities(indice, passwordMangleTabRegexs, nbRegexs):
@@ -72,7 +71,7 @@ def generateRegexsPermutsPossibilities(indice, passwordMangleTabRegexs, nbRegexs
 
 def dictionary(zfile, dic):
     #TODO: check utility of back file generated
-    back = '.' + zfile +'.data'
+    global re
     password = ''
     try:
         f = open(back,'r')
@@ -98,33 +97,42 @@ def dictionary(zfile, dic):
         flag = False
     if not flag:
         dlist = dictionaryFile.readlines()
-        for i in xrange(start,len(dlist)):
+        nbLines = len(dlist)
+        for i in range(start,nbLines):
+
+            percentProgress = (i / nbLines * 100)
+
+            if (100 % percentProgress == 0):
+                print (str(percentProgress) + '%')
+
+            passwordToTest = re.sub('\n', '', dlist[i]) #default delete new lines
             try:
-                flag = generateRegexsPermutsPossibilities(0, passwordMangleTabRegexs, len(passwordMangleTabRegexs))
-                #flag = manglePasswordAndExtract(zfile, dlist[i])
-                if flag:
-                    password = dlist[i]
-                    break
+                #print ('\n\nNew password to mangle : ' + passwordToTest, end='')
+                generatorRegexs = generateRegexsPermutsPossibilities(0, passwordMangleTabRegexs, len(passwordMangleTabRegexs))
+                for generatedRegexsTab in generatorRegexs:
+                    #print ('\nchange regex order and retry...')
+                    passwordFounded = manglePasswordAndExtract(zfile, passwordToTest, generatedRegexsTab)
+                    if passwordFounded:
+                        return passwordFounded
             except KeyboardInterrupt:
                 exit(0)
-    if not flag:
-        print 'Password Not Found!!!'
-        exit(0)
-    f = open(back,'w')
-    f.write('pwd:' + password)
-    f.close()
-    return password
+    print ('Password Not Found!!!')
+    return
 
 
 if __name__ == '__main__':
     arg = sys.argv[1:]
     if arg[1] == '-d':
-        print 'Processing...'
+        print ('Processing...')
         fl = arg[2].split('=')[1]
-        #TEMP
         password = dictionary(arg[0],fl)
-        #FINTEMP
 
-        print 'Password is "%s" without quotes.' %password
+        if (password):
+            back = '.passwordfound.data'
+            f = open(back,'w')
+            f.write('pwd:' + password)
+            f.close()
+
+        exit(0)
     else:
         exit(0)
